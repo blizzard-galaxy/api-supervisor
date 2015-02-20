85483ea1e176e9cfd8e62c476991bb6379effd91
Petre Pătraşc <petre@dreamlabs.ro>
2015-02-21 00:11:44 +0200

    Add deserialization support for rewards

---
+++ src/Entity/Starcraft/Player/Rewards.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace BlizzardGalaxy\ApiSupervisor\Entity\Starcraft\Player;
+
+use BlizzardGalaxy\ApiSupervisor\Test\Entity\ApiSupervisorEntityInterface;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class Rewards
+ *
+ * @package BlizzardGalaxy\ApiSupervisor\Entity\Starcraft\Player
+ * @author  Petre Pătrașc <petre@dreamlabs.ro>
+ */
+class Rewards implements ApiSupervisorEntityInterface
+{
+    /**
+     * @var array
+     *
+     * @JMS\SerializedName("selected")
+     * @JMS\Type("array<integer>")
+     */
+    protected $selected = array();
+
+    /**
+     * @var array
+     *
+     * @JMS\SerializedName("earned")
+     * @JMS\Type("array<integer>")
+     */
+    protected $earned = array();
+
+    /**
+     * @return array
+     */
+    public function getSelected()
+    {
+        return $this->selected;
+    }
+
+    /**
+     * @param array $selected
+     *
+     * @return $this
+     */
+    public function setSelected($selected)
+    {
+        $this->selected = $selected;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getEarned()
+    {
+        return $this->earned;
+    }
+
+    /**
+     * @param array $earned
+     *
+     * @return $this
+     */
+    public function setEarned($earned)
+    {
+        $this->earned = $earned;
+
+        return $this;
+    }
+
+
+}
