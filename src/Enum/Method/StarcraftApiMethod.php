a649e1552a1c5dad90dbf9a3d64bdbe6cf98841e
Petre Pătraşc <petre@dreamlabs.ro>
2015-02-23 01:01:03 +0200

    Remove ArrayInterface constraint for Method enums

--- src/Enum/Method/StarcraftApiMethod.php
+++ src/Enum/Method/StarcraftApiMethod.php
@@ -3,33 +3,16 @@
 
 namespace BlizzardGalaxy\ApiSupervisor\Enum\Method;
 
-use BlizzardGalaxy\ApiSupervisor\Enum\ArrayEnumInterface;
-
 /**
  * Keeps a list of all the starcraft methods that can be called.
  *
  * @package BlizzardGalaxy\ApiSupervisor\Enum\Method
  * @author  Petre Pătrașc <petre@dreamlabs.ro>
  */
-class StarcraftApiMethod implements ArrayEnumInterface
+class StarcraftApiMethod
 {
     const PLAYER = 'profile';
     const LADDER = 'ladder';
     const ACHIEVEMENTS = 'achievements';
     const REWARDS = 'rewards';
-
-    /**
-     * Get all of the entries as an array.
-     *
-     * @return array
-     */
-    public static function getAllAsArray()
-    {
-        return [
-            self::PLAYER,
-            self::LADDER,
-            self::ACHIEVEMENTS,
-            self::REWARDS,
-        ];
-    }
 }
